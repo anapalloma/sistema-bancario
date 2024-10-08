0fb0a81c0a81e7da66ccb3f199bcc804bca9dbfc
Ana Palloma <anapallomarocha@gmail.com>
2024-10-08 12:32:12 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,28 @@
+menu = """
+
+[1] Depositar
+[2] Sacar
+[3] Extrato
+[4] Sair
+
+=> """
+
+saldo = numero_saques = 0
+limite = 500
+extrato = ""
+LIMITE_SAQUES = 3
+
+while True:
+    opcao = int(input(menu))
+
+    if opcao == 1:
+        pass
+    elif opcao == 2:
+        pass
+    elif opcao == 3:
+        pass
+    elif opcao == 4:
+        break
+    else:
+        print('Opção inválida. Tente novamente.')
+    
