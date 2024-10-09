3c2b00b52f63a14c8257ca1ea27848f7ef8ac70b
Ana Palloma <anapallomarocha@gmail.com>
2024-10-09 15:32:10 -0300

    feat(depositar): add de função de depósito para atualizar saldo da conta

--- main.py
+++ main.py
@@ -1,3 +1,41 @@
+# Variáveis globais
+saldo = numero_saques = 0
+limite = 500
+extrato = []
+LIMITE_SAQUES = 3
+        
+
+def validacao():
+    while True:
+        opcao = int(input('Deseja continuar? [1] Sim, [2] Não '))
+        if opcao in [1, 2]:
+            return opcao # Retorna o valor para onde a função foi chamada
+        else:
+            print('Resposta inválida. Tente novamente!')
+
+def depositar():
+    global saldo
+    while True:
+        valor = float(input('Informe o valor do depósito: '))
+
+        if valor > 0:
+            saldo += valor
+            extrato.append(f'Depósito de: R$ {valor:.2f}')
+            print(extrato)
+        
+        else:
+            print('Não é possível depositar valores negativos.')
+         
+        opcao = validacao()
+        if opcao == 2:
+            break
+          
+        
+       
+
+
+
+
 menu = """
 
 [1] Depositar
@@ -7,22 +45,16 @@ menu = """
 
 => """
 
-saldo = numero_saques = 0
-limite = 500
-extrato = ""
-LIMITE_SAQUES = 3
-
 while True:
     opcao = int(input(menu))
-
     if opcao == 1:
-        pass
+        depositar()
+        
     elif opcao == 2:
-        pass
+        sacar()
     elif opcao == 3:
-        pass
+        extrato()
     elif opcao == 4:
         break
     else:
         print('Opção inválida. Tente novamente.')
-    
