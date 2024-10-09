4fb2266523546aad004f6eb7a751fc7a5d7e1eda
Ana Palloma <anapallomarocha@gmail.com>
2024-10-09 15:58:34 -0300

    feat(sacar): add de função de saque

--- main.py
+++ main.py
@@ -2,6 +2,8 @@
 saldo = numero_saques = 0
 limite = 500
 extrato = []
+
+# Variável imutável
 LIMITE_SAQUES = 3
         
 
@@ -29,12 +31,35 @@ def depositar():
         opcao = validacao()
         if opcao == 2:
             break
-          
+
+def sacar():
+    global saldo
+    global limite
+    global numero_saques
+    global LIMITE_SAQUES
+
+    while True:
+        valor = float(input('Informe o valor do saque: '))
+
+        if (valor < limite) and (valor <saldo) and (numero_saques < LIMITE_SAQUES):
+            saldo -= valor;
+            extrato.append(f'Saque: R$ {valor:.2f}')
+            numero_saques += 1
         
-       
+        elif valor > limite:
+            print('Operação não permitida. Valor excede ao limite disponível na conta.')
+        
+        elif valor > saldo:
+            print('Operação não permitida. Saldo insuficiente.')
+        
+        elif numero_saques > LIMITE_SAQUES:
+            print('Operação não permitida. Quantidade diária de saques excedida.')
+        print(extrato)
 
-
-
+        opcao = validacao()
+        if opcao == 2:
+            break
+          
 
 menu = """
 
@@ -49,7 +74,6 @@ while True:
     opcao = int(input(menu))
     if opcao == 1:
         depositar()
-        
     elif opcao == 2:
         sacar()
     elif opcao == 3:
